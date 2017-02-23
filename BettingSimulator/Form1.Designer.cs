cdcc657901d1aeb6c6de90c87a37a454f37840d2
ASutyak <asutyak@umich.edu>
2017-02-23 10:38:22 -0500

    Add project files.

---
+++ BettingSimulator/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace BettingSimulator
+{
+    partial class lossesValue
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.enterPowerball = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.losses = new System.Windows.Forms.Label();
+            this.lossDisplay = new System.Windows.Forms.TextBox();
+            this.power6 = new System.Windows.Forms.TextBox();
+            this.winPower6 = new System.Windows.Forms.TextBox();
+            this.power1 = new System.Windows.Forms.TextBox();
+            this.power2 = new System.Windows.Forms.TextBox();
+            this.power3 = new System.Windows.Forms.TextBox();
+            this.power4 = new System.Windows.Forms.TextBox();
+            this.power5 = new System.Windows.Forms.TextBox();
+            this.winPower5 = new System.Windows.Forms.TextBox();
+            this.winPower4 = new System.Windows.Forms.TextBox();
+            this.winPower3 = new System.Windows.Forms.TextBox();
+            this.winPower2 = new System.Windows.Forms.TextBox();
+            this.winPower1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.winningHorse = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // enterPowerball
+            // 
+            this.enterPowerball.Location = new System.Drawing.Point(58, 155);
+            this.enterPowerball.Name = "enterPowerball";
+            this.enterPowerball.Size = new System.Drawing.Size(75, 23);
+            this.enterPowerball.TabIndex = 2;
+            this.enterPowerball.Text = "Enter Num";
+            this.enterPowerball.UseVisualStyleBackColor = true;
+            this.enterPowerball.Click += new System.EventHandler(this.enterPowerball_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Your Numbers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 17);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Powerball $2 A Play";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Winning Numbers";
+            // 
+            // losses
+            // 
+            this.losses.AutoSize = true;
+            this.losses.Location = new System.Drawing.Point(437, 11);
+            this.losses.Name = "losses";
+            this.losses.Size = new System.Drawing.Size(57, 17);
+            this.losses.TabIndex = 21;
+            this.losses.Text = "Losses:";
+            // 
+            // lossDisplay
+            // 
+            this.lossDisplay.Location = new System.Drawing.Point(500, 8);
+            this.lossDisplay.Name = "lossDisplay";
+            this.lossDisplay.Size = new System.Drawing.Size(76, 22);
+            this.lossDisplay.TabIndex = 22;
+            // 
+            // power6
+            // 
+            this.power6.Location = new System.Drawing.Point(204, 133);
+            this.power6.Name = "power6";
+            this.power6.Size = new System.Drawing.Size(33, 22);
+            this.power6.TabIndex = 23;
+            this.power6.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // winPower6
+            // 
+            this.winPower6.Location = new System.Drawing.Point(204, 77);
+            this.winPower6.Name = "winPower6";
+            this.winPower6.Size = new System.Drawing.Size(33, 22);
+            this.winPower6.TabIndex = 24;
+            // 
+            // power1
+            // 
+            this.power1.Location = new System.Drawing.Point(11, 133);
+            this.power1.Name = "power1";
+            this.power1.Size = new System.Drawing.Size(33, 22);
+            this.power1.TabIndex = 25;
+            // 
+            // power2
+            // 
+            this.power2.Location = new System.Drawing.Point(50, 133);
+            this.power2.Name = "power2";
+            this.power2.Size = new System.Drawing.Size(33, 22);
+            this.power2.TabIndex = 26;
+            // 
+            // power3
+            // 
+            this.power3.Location = new System.Drawing.Point(89, 133);
+            this.power3.Name = "power3";
+            this.power3.Size = new System.Drawing.Size(33, 22);
+            this.power3.TabIndex = 27;
+            // 
+            // power4
+            // 
+            this.power4.Location = new System.Drawing.Point(128, 133);
+            this.power4.Name = "power4";
+            this.power4.Size = new System.Drawing.Size(33, 22);
+            this.power4.TabIndex = 28;
+            // 
+            // power5
+            // 
+            this.power5.Location = new System.Drawing.Point(165, 133);
+            this.power5.Name = "power5";
+            this.power5.Size = new System.Drawing.Size(33, 22);
+            this.power5.TabIndex = 29;
+            // 
+            // winPower5
+            // 
+            this.winPower5.Location = new System.Drawing.Point(165, 77);
+            this.winPower5.Name = "winPower5";
+            this.winPower5.Size = new System.Drawing.Size(33, 22);
+            this.winPower5.TabIndex = 30;
+            // 
+            // winPower4
+            // 
+            this.winPower4.Location = new System.Drawing.Point(128, 77);
+            this.winPower4.Name = "winPower4";
+            this.winPower4.Size = new System.Drawing.Size(33, 22);
+            this.winPower4.TabIndex = 31;
+            // 
+            // winPower3
+            // 
+            this.winPower3.Location = new System.Drawing.Point(89, 77);
+            this.winPower3.Name = "winPower3";
+            this.winPower3.Size = new System.Drawing.Size(33, 22);
+            this.winPower3.TabIndex = 32;
+            // 
+            // winPower2
+            // 
+            this.winPower2.Location = new System.Drawing.Point(50, 77);
+            this.winPower2.Name = "winPower2";
+            this.winPower2.Size = new System.Drawing.Size(33, 22);
+            this.winPower2.TabIndex = 33;
+            // 
+            // winPower1
+            // 
+            this.winPower1.Location = new System.Drawing.Point(12, 77);
+            this.winPower1.Name = "winPower1";
+            this.winPower1.Size = new System.Drawing.Size(33, 22);
+            this.winPower1.TabIndex = 34;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Horse 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 36;
+            this.button2.Text = "Horse 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(195, 297);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 37;
+            this.button3.Text = "Horse 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(286, 297);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 38;
+            this.button4.Text = "Horse 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(380, 297);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 39;
+            this.button5.Text = "Horse 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(152, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(158, 17);
+            this.label4.TabIndex = 45;
+            this.label4.Text = "Horse Race! $10 A Play";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // winningHorse
+            // 
+            this.winningHorse.Location = new System.Drawing.Point(144, 260);
+            this.winningHorse.Name = "winningHorse";
+            this.winningHorse.Size = new System.Drawing.Size(166, 22);
+            this.winningHorse.TabIndex = 46;
+            // 
+            // lossesValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 436);
+            this.Controls.Add(this.winningHorse);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.winPower1);
+            this.Controls.Add(this.winPower2);
+            this.Controls.Add(this.winPower3);
+            this.Controls.Add(this.winPower4);
+            this.Controls.Add(this.winPower5);
+            this.Controls.Add(this.power5);
+            this.Controls.Add(this.power4);
+            this.Controls.Add(this.power3);
+            this.Controls.Add(this.power2);
+            this.Controls.Add(this.power1);
+            this.Controls.Add(this.winPower6);
+            this.Controls.Add(this.power6);
+            this.Controls.Add(this.lossDisplay);
+            this.Controls.Add(this.losses);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.enterPowerball);
+            this.Name = "lossesValue";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button enterPowerball;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label losses;
+        private System.Windows.Forms.TextBox lossDisplay;
+        private System.Windows.Forms.TextBox power6;
+        private System.Windows.Forms.TextBox winPower6;
+        private System.Windows.Forms.TextBox power1;
+        private System.Windows.Forms.TextBox power2;
+        private System.Windows.Forms.TextBox power3;
+        private System.Windows.Forms.TextBox power4;
+        private System.Windows.Forms.TextBox power5;
+        private System.Windows.Forms.TextBox winPower5;
+        private System.Windows.Forms.TextBox winPower4;
+        private System.Windows.Forms.TextBox winPower3;
+        private System.Windows.Forms.TextBox winPower2;
+        private System.Windows.Forms.TextBox winPower1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox winningHorse;
+    }
+}
+
---
+++ BettingSimulator/Form1.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BettingSimulator
+{
+    public partial class lossesValue : Form
+    {
+        int[] winningpowerballNumbers;
+        int[] yourPowerballNumbers;
+        int[] horses;
+        Random rand;
+        int totalLosses;
+        
+
+        public lossesValue()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rand = new Random();
+            winningpowerballNumbers = new int[6] {0, 0, 0, 0, 0, 0 };
+            yourPowerballNumbers = new int[6] {0, 0, 0, 0, 0, 0 };
+            horses = new int[5] { 0, 0, 0, 0, 0 };
+            int totalLosses = 0;
+        }
+
+        private void enterPowerball_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 2;
+            int numReg = 0;
+            bool redball = false;
+
+            for (int i = 0; i < yourPowerballNumbers.Length; i++)
+            {
+                if (i == 5)
+                {
+                    yourPowerballNumbers[5] = rand.Next(1, 27); // 1 to 26 for red
+                }
+                else
+                yourPowerballNumbers[i] = rand.Next(1, 60); //from 1 to 59
+            }
+            for (int i = 0; i < winningpowerballNumbers.Length; i++)
+            {
+                if (i == 5)
+                {
+                    winningpowerballNumbers[5] = rand.Next(1, 27); // 1 to 26 for red
+                }
+                else
+                    winningpowerballNumbers[i] = rand.Next(1, 60); //from 1 to 59
+            }
+            winPower1.Text = winningpowerballNumbers[0].ToString();
+            winPower2.Text = winningpowerballNumbers[1].ToString();
+            winPower3.Text = winningpowerballNumbers[2].ToString();
+            winPower4.Text = winningpowerballNumbers[3].ToString();
+            winPower5.Text = winningpowerballNumbers[4].ToString();
+            winPower6.Text = winningpowerballNumbers[5].ToString();
+
+            power1.Text = yourPowerballNumbers[0].ToString();
+            power2.Text = yourPowerballNumbers[1].ToString();
+            power3.Text = yourPowerballNumbers[2].ToString();
+            power4.Text = yourPowerballNumbers[3].ToString();
+            power5.Text = yourPowerballNumbers[4].ToString();
+            power6.Text = yourPowerballNumbers[5].ToString();
+
+            if(yourPowerballNumbers[0] == winningpowerballNumbers[0])
+            {
+                numReg++;
+            }
+            if(yourPowerballNumbers[1] == winningpowerballNumbers[1])
+            {
+                numReg++;
+            }
+            if (yourPowerballNumbers[2] == winningpowerballNumbers[2])
+            {
+                numReg++;
+            }
+            if (yourPowerballNumbers[3] == winningpowerballNumbers[3])
+            {
+                numReg++;
+            }
+            if(yourPowerballNumbers[4] == winningpowerballNumbers[4])
+            {
+                numReg++;
+            }
+            if(yourPowerballNumbers[5] == winningpowerballNumbers[5])
+            {
+                redball = true;
+            }
+            if (redball == true)
+            {
+                if (numReg == 5)
+                {
+                    totalLosses = totalLosses + 300000000;
+                }
+                else if (numReg == 4)
+                {
+                    totalLosses = totalLosses + 50000;
+                }
+                else if (numReg == 3)
+                {
+                    totalLosses = totalLosses + 100;
+                }
+                else if (numReg == 2)
+                {
+                    totalLosses = totalLosses + 7;
+                }
+                else
+                {
+                    totalLosses = totalLosses + 4;
+                }
+                 
+            }
+            else if(numReg == 5)
+            {
+                totalLosses = totalLosses + 1000000;
+            }
+            else if (numReg == 4)
+            {
+                totalLosses = totalLosses + 100;
+            }
+            else if (numReg == 3)
+            {
+                totalLosses = totalLosses + 7;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 10;
+            int winner;
+            winner = horserace();
+            winningHorse.Text = "Horse " + winner + " Wins!";
+
+            if (winner == 1)
+            {
+                totalLosses = totalLosses + 20;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 10;
+            int winner;
+            winner = horserace();
+            winningHorse.Text = "Horse " + winner + " Wins!";
+
+            if (winner == 2)
+            {
+                totalLosses = totalLosses + 20;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 10;
+            int winner;
+            winner = horserace();
+            winningHorse.Text = "Horse " + winner + " Wins!";
+
+            if (winner == 3)
+            {
+                totalLosses = totalLosses + 20;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 10;
+            int winner;
+            winner = horserace();
+            winningHorse.Text = "Horse " + winner + " Wins!";
+
+            if (winner == 4)
+            {
+                totalLosses = totalLosses + 20;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            totalLosses = totalLosses - 10;
+            int winner;
+            winner = horserace();
+            winningHorse.Text = "Horse " + winner + " Wins!";
+
+            if (winner == 5)
+            {
+                totalLosses = totalLosses + 20;
+            }
+            lossDisplay.Text = totalLosses.ToString();
+        }
+        private int horserace()
+        {
+            int winNum = 0;
+            for (int i = 0; i < horses.Length; i++)
+            {
+                horses[i] = rand.Next(1, 25); // 1 to 25 for speed
+            }
+            for (int j = 0; j < 5; j++)
+            {
+                if (j != 4)
+                {
+                    if (horses[j] > horses[j + 1])
+                    {
+                        winNum = j;
+                    }
+                }
+            }
+            return winNum + 1;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
